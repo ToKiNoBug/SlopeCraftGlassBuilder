5456a666656daef7f21084e588ac8cbfa9a045b9
时之虫 <62002934+ToKiNoBug@users.noreply.github.com>
2021-09-17 19:44:43 +0800

    添加声明

--- PrimGlassBuilder.cpp
+++ PrimGlassBuilder.cpp
@@ -1,8 +1,31 @@
+/*
+ Copyright © 2021  TokiNoBug
+This file is part of SlopeCraft.
+
+    SlopeCraft is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SlopeCraft is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SlopeCraft.  If not, see <https://www.gnu.org/licenses/>.
+
+    Contact with me:
+    github:https://github.com/ToKiNoBug
+    bilibili:https://space.bilibili.com/351429231
+*/
+
 #include "PrimGlassBuilder.h"
 
 const ARGB airColor=qRgb(255,255,255);
 const ARGB targetColor=qRgb(0,0,0);
 const ARGB glassColor=qRgb(192,192,192);
+
 
 double randD(){
     static std::default_random_engine generator(time(0));
@@ -70,7 +93,11 @@ PrimGlassBuilder::PrimGlassBuilder(QObject *parent) : QObject(parent)
 PrimGlassBuilder::PrimGlassBuilder()
 #endif
 {
-
+#ifndef WITH_QT
+    progressRangeSet=defaultProgressRangeSet;
+    progressAdd=defaultProgressAdd;
+    keepAwake=defaultKeepAwake;
+#endif
 }
 
 glassMap PrimGlassBuilder::makeBridge(const TokiMap & _targetMap,
--- PrimGlassBuilder.h
+++ PrimGlassBuilder.h
@@ -1,3 +1,25 @@
+/*
+ Copyright © 2021  TokiNoBug
+This file is part of SlopeCraft.
+
+    SlopeCraft is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SlopeCraft is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SlopeCraft.  If not, see <https://www.gnu.org/licenses/>.
+
+    Contact with me:
+    github:https://github.com/ToKiNoBug
+    bilibili:https://space.bilibili.com/351429231
+*/
+
 #ifndef PRIMGLASSBUILDER_H
 #define PRIMGLASSBUILDER_H
 
@@ -19,14 +41,16 @@
 #include <ctime>
 #include "WaterItem.h"
 
-#include <QtConcurrent>
 #ifdef WITH_QT
     #include <QObject>
     #include <QRgb>
+    #include <QDebug>
 #else
-typedef unsigned char uchar;
-typedef unsigned short ushort;
-typedef unsigned int uint;
+    #define qDebug()<< std::cerr<<
+    #define emit ;
+    typedef unsigned char uchar;
+    typedef unsigned short ushort;
+    typedef unsigned int uint;
 #endif
 
 
@@ -80,7 +104,9 @@ signals:
     void progressAdd(int);
     void keepAwake();
 #else
-
+    void (*progressRangeSet)(int,int,int);
+    void (*progressAdd)(int);
+    void (*keepAwake)();
 #endif
 private:
     std::vector<TokiPos> targetPoints;
@@ -96,4 +122,8 @@ private:
 
 EImage TokiMap2EImage(const TokiMap&);
 double randD();
+
+void defaultProgressRangeSet(int,int,int){};
+void defaultProgressAdd(int){};
+void defaultKeepAwake(){};
 #endif // PRIMGLASSBUILDER_H
